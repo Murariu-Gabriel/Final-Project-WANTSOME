61aaad1ae265786d5a8d0cc6b85ee2b4ddb6bf92
Gabriel <murariugabi23@gmail.com>
2023-05-18 22:03:32 +0300

    Made some small changes

--- dashboard.js
+++ dashboard.js
@@ -8,7 +8,7 @@ hamburgerButton.addEventListener("click", () => {
 
   if (hamburgerButton.classList.contains("toggle-close")) {
     hamburgerButton.innerHTML = `
-        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
+      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="25" width="25" xmlns="http://www.w3.org/2000/svg"><path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"></path></svg>
           `
   } else {
     hamburgerButton.innerHTML = `
