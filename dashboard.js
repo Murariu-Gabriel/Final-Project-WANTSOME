8b56b9198f552b35a557370e534979d5504ec405
Gabriel <murariugabi23@gmail.com>
2023-05-23 16:52:01 +0300

    Made some changed, fixed issues added some animations

--- dashboard.js
+++ dashboard.js
@@ -1,24 +1,50 @@
 const headerNav = document.getElementById("header-nav")
 const hamburgerButton = document.getElementById("h-button")
+const closeButton = document.getElementById("x-button")
 
 hamburgerButton.addEventListener("click", () => {
   headerNav.classList.toggle("nav-toggle")
+  hamburgerButton.classList.toggle("display-none")
+  closeButton.classList.toggle("display-none")
 
-  hamburgerButton.classList.toggle("toggle-close")
-
-  if (hamburgerButton.classList.contains("toggle-close")) {
-    hamburgerButton.innerHTML = `
-      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
-        <path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z">
-        </path>
-      </svg>
-          `
-  } else {
-    hamburgerButton.innerHTML = `
-      <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
-        <g fill-rule="evenodd">
-          <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
-        </g>
-      </svg>         `
-  }
 })
+
+closeButton.addEventListener("click", () => {
+    closeButton.classList.toggle("display-none")
+    hamburgerButton.classList.toggle("display-none")
+    headerNav.classList.toggle("nav-toggle")
+})
+
+
+
+
+
+
+
+
+// IN cazul in care te razgandesti si vrei sa faci toggle cu DOM
+
+  // if (hamburgerButton.classList.contains("toggle-close")) {
+  //   hamburgerButton.innerHTML = `
+  //     <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+  //       <path d="M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z">
+  //       </path>
+  //     </svg>
+  //         `
+  // } else {
+  //   hamburgerButton.innerHTML = `
+  //     <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+  //       <g fill-rule="evenodd">
+  //         <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
+  //       </g>
+  //     </svg>         `
+  // }
+// })
+
+
+
+
+
+
+
+
