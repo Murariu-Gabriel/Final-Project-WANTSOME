db43a99504286a69dcb0dd514da21f9fcd8b3bb4
Gabriel <murariugabi23@gmail.com>
2023-06-22 20:46:34 +0300

    almost finished search toggle style

--- scripts/search-page.js
+++ scripts/search-page.js
@@ -3,9 +3,7 @@ const inputContainer = document.getElementById("input-container")
 const dummy = document.getElementById("dummy")
 const searchInput = document.getElementById("search-input")
 const formContent = document.getElementById("form-content")
-
-// const dummies = formContent.querySelectorAll("span")
-// console.log(dummies)
+const searchResults = document.getElementById("search-results")
 
 searchInput.addEventListener("click", (e) => {
     searchContainer.classList.toggle("overlay2")
@@ -13,16 +11,7 @@ searchInput.addEventListener("click", (e) => {
     inputContainer.classList.toggle("search-animation")
     formContent.classList.toggle("contend-width")
     dummy.classList.toggle("hide")
-    // if (window.innerWidth > 550) {
-    //   for (const dummy of dummies) {
-    //     dummy.classList.toggle("hide")
-    //     if (dummy.classList.contains("dummy-list")) {
-    //       dummy.classList.toggle("flex")
-    //     }
-    //   }
-    // }
-    // const screenWidth = window.innerWidth
-    // console.log(window.innerWidth > 550)
+    searchResults.classList.toggle("hide")
 
     // e.preventDefault()
     // if(searchContainer.classList.contains("overlay2")){
